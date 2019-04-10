4484b3ca0c4a7bb5192e0607b569662dfb8a8acd
Giuseppe Trapani <giuseppe.trapani@imtlucca.it>
2019-04-10 14:40:21 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CMSSpreadPricing',
+      version='0.1',
+      description='Funzioni per pricing di CMS spread e convexity'
+      url='http://github.com/storborg/funniest',
+      author='Giuseppe Trapani',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
